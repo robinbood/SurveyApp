efaeb4f0e42582c8f5850fc683653d0a6376480f
robinbood <mercedesfarts@gmail.com>
2025-08-14 11:13:30 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import { APITester } from "./APITester";
 import "./index.css";
 
-import logo from "./SVG/anime.svg";
 
 export function App() {
   return (
-    <div className="app">
-      <div className="logo-container">
-        <img src={logo} alt="Bun Logo" className="logo bun-logo" />
-      </div>
-
-      <h1>Bun + React</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-      <APITester />
-    </div>
+    <></>
   );
 }
 
